213befdcece5340995e84e7ec6116924dd590972
Kacper Popczyński <popkacper@gmail.com>
2021-04-29 22:04:24 +0200

    Add CoreModule

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,21 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppInitializer, AppInitializerProvider } from './app-initializer';
+
+@NgModule({
+    declarations: [],
+    imports: [CommonModule],
+    exports: [],
+    providers: [],
+})
+export class CoreModule {
+    public static forRoot(): ModuleWithProviders<CoreModule> {
+        return {
+            ngModule: CoreModule,
+            providers: [
+                AppInitializer,
+                AppInitializerProvider,
+            ],
+        };
+    }
+}
